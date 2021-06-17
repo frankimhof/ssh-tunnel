165854d14f86218e5b10edefd2d41a1b1ac056b5
frankimhof <imhof.frank@gmail.com>
2021-06-17 22:23:49 +0200

    some renaming and improved printing of information

--- classic/serverAndRemoteSetup.sh
+++ classic/serverAndRemoteSetup.sh
@@ -22,8 +22,8 @@ if [ $? -eq 0 ]; then
     echo "Stopping container: ${CONTAINER}"
     evaldbg "docker stop ${CONTAINER} -t 0"
 fi
-CONTAINER="ssh-remote"
-evaldbg "docker ps | grep ssh-remote"
+CONTAINER="remote-webserver"
+evaldbg "docker ps | grep remote-webserver"
 if [ $? -eq 0 ]; then
     echo "Stopping container: ${CONTAINER}"
     evaldbg "docker stop ${CONTAINER} -t 0"
@@ -31,47 +31,54 @@ fi
 
 # start containers
 docker run ${DOCKER_OPTIONS}  -p ${SSH_PORT}:${SSH_PORT} --name ssh-server ${IMAGE_TAG}
-docker run ${DOCKER_OPTIONS} --name ssh-remote ${IMAGE_TAG}
+docker run ${DOCKER_OPTIONS} --name remote-webserver ${IMAGE_TAG}
+REMOTEIP=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' remote-webserver)
+
 # create host keys for the ssh-server container
 echo "### Creating host keys (rsa) for the ssh-server container..."
 evaldbg "docker exec -u ${USER} ssh-server /bin/sh -c 'ssh-keygen -t rsa -f ~/.ssh/ssh_host_rsa_key -N \"\" -q'"
 if [[ $? -eq 0 ]]; then
-    echo "### [ OK ] ### Generation of host keys was successfully."
+    echo -e "### [ OK ] ###\n\n"
 else
     echo "### [FAIL] ### Error while generating host keys."
+    exit 1
 fi
 
 echo "### Starting the ssh server in the ssh-server container..."
 # start ssh server in the ssh-server container
 evaldbg "docker exec -u ${USER} ssh-server /bin/sh -c '/usr/sbin/sshd -f sshd_config'"
 if [[ $? -eq 0 ]]; then
-    echo "### [ OK ] ### Setup of ssh-server was successfull."
+    echo -e "### [ OK ] ###\n\n"
 else
     echo "### [FAIL] ### Error while starting the ssh server."
+    exit 1
 fi
 
-# WEBSERVER SETUP (ssh-remote)
+# WEBSERVER SETUP (remote-webserver)
 # create folders and give rights to ${USER}
-docker exec -u root -t ssh-remote /bin/sh -c "mkdir /var/run/lighttpd && chown -R ${USER}:${USER} /var/run/lighttpd"
-docker exec -u root -t ssh-remote /bin/sh -c "mkdir -p /var/www/webserver/html && chown -R ${USER}:${USER} /var/www/webserver"
-docker exec -u root -t ssh-remote /bin/sh -c "chown -R ${USER}:${USER} /var/log/lighttpd"
-docker exec -u root -t ssh-remote /bin/sh -c "cp /etc/lighttpd/lighttpd.conf lighttpd.conf && chown ${USER}:${USER} lighttpd.conf"
+docker exec -u root -t remote-webserver /bin/sh -c "mkdir /var/run/lighttpd && chown -R ${USER}:${USER} /var/run/lighttpd"
+docker exec -u root -t remote-webserver /bin/sh -c "mkdir -p /var/www/webserver/html && chown -R ${USER}:${USER} /var/www/webserver"
+docker exec -u root -t remote-webserver /bin/sh -c "chown -R ${USER}:${USER} /var/log/lighttpd"
+docker exec -u root -t remote-webserver /bin/sh -c "cp /etc/lighttpd/lighttpd.conf lighttpd.conf && chown ${USER}:${USER} lighttpd.conf"
 # change lighttpd.conf such that
 # -html files will be located at /var/www/webserver/html
 # -log files will be located at /var/log/lighttpd
 # -lighttpd.pid file will be located in /var/run/lighttpd/
-docker exec -t ssh-remote /bin/sh -c "sed -i 's/\/run\/lighttpd.pid/\/var\/run\/lighttpd\/lighttpd.pid/' lighttpd.conf"
-docker exec -t ssh-remote /bin/sh -c "sed -i 's/\/var\/www\/localhost/\/var\/www\/webserver/' lighttpd.conf"
-docker exec -t ssh-remote /bin/sh -c "sed -i 's/htdocs/html/' lighttpd.conf"
-docker exec -t ssh-remote /bin/sh -c "sed -i '42s/^/#/' lighttpd.conf" #comment out the inclusion of mime-types.conf
+docker exec -t remote-webserver /bin/sh -c "sed -i 's/\/run\/lighttpd.pid/\/var\/run\/lighttpd\/lighttpd.pid/' lighttpd.conf"
+docker exec -t remote-webserver /bin/sh -c "sed -i 's/\/var\/www\/localhost/\/var\/www\/webserver/' lighttpd.conf"
+docker exec -t remote-webserver /bin/sh -c "sed -i 's/htdocs/html/' lighttpd.conf"
+docker exec -t remote-webserver /bin/sh -c "sed -i '42s/^/#/' lighttpd.conf" #comment out the inclusion of mime-types.conf
 
-# create the simple html file
-docker exec -t ssh-remote /bin/sh -c "echo 'Congrats, you reached the webpage of the remote webserver!' > /var/www/webserver/html/index.html"
-# run the webserver
-echo "### Setting up the webserver in the ssh-remote container..."
-evaldbg "docker exec -t ssh-remote /usr/sbin/lighttpd -f /home/${USER}/lighttpd.conf"
+# create a simple html file
+docker exec -t remote-webserver /bin/sh -c "echo 'Congrats, you reached the webpage of the remote webserver!' > /var/www/webserver/html/index.html"
+
+# start the webserver
+echo "### Setting up the webserver in the remote-webserver container..."
+evaldbg "docker exec -t remote-webserver /usr/sbin/lighttpd -f /home/${USER}/lighttpd.conf"
 if [[ $? -eq 0 ]]; then
-    echo "### [ OK ] ### Set up of webserver was successfull."
+    echo -e "### [ OK ] ###\n\n"
+    echo -e "### ssh server and remote (webserver) succsessfully set up.\n### Start the ssh-client container with the following command (on another machine):\n./clientSetup.sh <SSH_SERVER_IP> <REMOTE_WEBSERVER_CONTAINER_IP>\n<SSH_SERVER_IP> = the ip address of the machine running the ssh-server container\n<REMOTE_WEBSERVER_CONTAINER_IP> = ${REMOTEIP}"
 else
     echo "### [FAIL] ### Error while starting the ssh server"
+    exit 1
 fi

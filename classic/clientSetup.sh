226b882a964f045467a6272abd43d7614bf00028
frankimhof <imhof.frank@gmail.com>
2021-06-18 18:25:07 +0200

    renaming some variables and adding instructions for fetching webpage through tunnel

--- classic/clientSetup.sh
+++ classic/clientSetup.sh
@@ -1,11 +1,10 @@
-
 if [ $# -lt 1 ]; then
   echo "USAGE: ./clientSetup.sh <SERVER_IP_ADDRESS> <REMOTE_WEBSERVER_CONTAINER_IP_ADDRESS>"
   echo "--> Please provide SERVER_IP_ADDRESS and REMOTE_WEBSERVER_CONTAINER_IP_ADDRESS as argument."
   exit 1
 else
-  SERVERIP=$1
-  REMOTEIP=$2
+  SERVER_IP=$1
+  REMOTE_IP=$2
 fi
 
 USER="testuser"
@@ -24,20 +23,20 @@ function evaldbg {
 }
 
 # stop/delete container if it was already running
-CONTAINER="ssh-client"
-evaldbg "docker ps | grep ${CONTAINER}"
+CONTAINER_NAME="ssh-client"
+evaldbg "docker ps | grep ${CONTAINER_NAME}"
 if [ $? -eq 0 ]; then
-    echo "Stopping container: ${CONTAINER}"
-    evaldbg "docker stop ${CONTAINER} -t 0"
+    echo "Stopping container: ${CONTAINER_NAME}"
+    evaldbg "docker stop ${CONTAINER_NAME} -t 0"
 fi
 
 # start the client container
-docker run ${DOCKER_OPTIONS} --name ssh-client ${IMAGE_TAG}
-CLIENTIP=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' ssh-client)
+docker run ${DOCKER_OPTIONS} --name ${CONTAINER_NAME} ${IMAGE_TAG}
+CLIENT_IP=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' ${CONTAINER_NAME})
 
 # generate the clients identity keys
-echo "### Generating the identity keys for ssh-client container..."
-evaldbg "docker exec -it ssh-client /bin/sh ./createIdentityKeys.sh"
+echo "### Generating the identity keys for ${CONTAINER_NAME} container..."
+evaldbg "docker exec -it ${CONTAINER_NAME} /bin/sh ./createIdentityKeys.sh"
 if [[ $? -eq 0 ]]; then
   echo -e "### [ OK ] ###\n\n"
 else
@@ -46,21 +45,27 @@ fi
 
 # pass the identity keys over to the server (ssh-server container)
 echo "### Sending the identity keys to the ssh server..."
-evaldbg "docker exec --user ${USER} ssh-client sshpass -p abcd ssh-copy-id ${USER}@${SERVERIP} -o StrictHostKeyChecking=no"
+evaldbg "docker exec -it -u ${USER} ${CONTAINER_NAME} sshpass -p ${PASSWORD} ssh-copy-id ${USER}@${SERVER_IP} -o StrictHostKeyChecking=no"
 if [[ $? -eq 0 ]]; then
   echo -e "### [ OK ] ###\n\n"
 else
-  echo "### [FAIL] ### Error while sending identity keys to ssh-server. Is the ssh-server running?\n Is the SERVERIP correct? (${SERVERIP})"
+  echo "### [FAIL] ### Error while sending identity keys to ssh-server. Is the ssh-server running?\n Is the SERVER_IP correct? (${SERVER_IP})"
 fi
 
-# create an ssh tunnel to the server. Forward local port 80 to remote port 80, enabling the client to reach the webpage at REMOTEIP:80.
+# create an ssh tunnel to the server. Forward local port 80 to remote port 80, enabling the client to reach the webpage at REMOTE_IP:80.
 echo "### Setting up the ssh tunnel..."
-evaldbg "docker exec -t ssh-client ssh -fNT -L ${CLIENTIP}:80:${REMOTEIP}:80 ${USER}@${SERVERIP}"
+evaldbg "docker exec -t ${CONTAINER_NAME} ssh -fNT -L ${CLIENT_IP}:80:${REMOTE_IP}:80 ${USER}@${SERVER_IP}"
 if [[ $? -eq 0 ]]; then
-  echo -e "### [ OK ] ### ssh tunnel successfully set up!\n### Try curl <CLIENT_CONTAINER_IP> to load the webpage from the webserver (which is running in the remote-webserver container).\n### <CLIENT_CONTAINER_IP> = ${CLIENTIP}"
+  echo -e "### [ OK ] ###\n\n"
+  echo -e "================================================================================"
+  echo -e "ssh tunnel (local port forwarding) successfully set up!\n"
+  echo -e "[${CONTAINER_NAME}]:80 was forwarded to [remote-webserver]:80"
+  echo -e "--------------------------------------------------------------------------------"
+  echo -e "Use one of the following options to reach the webserver and load the web page:"
+  echo -e "--> (on this machine): open up a webbrowser and visit localhost"
+  echo -e "--> (on this machine): curl localhost"
+  echo -e "--> (inside the [${CONTAINER_NAME}] container): curl ${CLIENT_IP}'\n"
+  echo -e ================================================================================"
 else
   echo -e "### [FAIL] ### Error while setting up the ssh tunnel."
 fi
-
-# entering the ssh-client container
-docker exec -it ssh-client /bin/sh
